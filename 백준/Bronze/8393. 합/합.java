35a63231755ac6e296fa7a1a7a082ef62702158a
Seohyun Kang <143741901+seohyunKangSouthKorea@users.noreply.github.com>
2023-09-01 20:34:41 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), sum=0;
+		for(int i=1;i<=n;i++) sum += i;
+		System.out.print(sum);
+		sc.close();
+	}	
+}
